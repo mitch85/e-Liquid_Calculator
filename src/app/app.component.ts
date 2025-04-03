227638b3599d6355d0afa680b3f1fcda4a25da14
Michael Marciniak <info@michael-marciniak.de>
2025-04-03 19:08:35 +0200

    Refactor Tailwind CSS integration and update app module for form handling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,36 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'e-Liquid_Calculator';
+
+  pg1: number = 50;
+  vg1: number = 50;
+
+  onPgChange1(): void {
+    this.pg1 = Math.min(100, Math.max(0, this.pg1 ?? 0));
+    this.vg1 = 100 - this.pg1;
+  }
+
+  onVgChange1(): void {
+    this.vg1 = Math.min(100, Math.max(0, this.vg1 ?? 0));
+    this.pg1 = 100 - this.vg1;
+  }
+
+  pg2: number = 50;
+  vg2: number = 50;
+
+  onPgChange2(): void {
+    this.pg2 = Math.min(100, Math.max(0, this.pg2 ?? 0));
+    this.vg2 = 100 - this.pg2;
+  }
+
+  onVgChange2(): void {
+    this.vg2 = Math.min(100, Math.max(0, this.vg2 ?? 0));
+    this.pg2 = 100 - this.vg2;
+  }
+
+  addFlavor() {}
 }
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+// const colors = require("tailwindcss/colors");
+module.exports = {
+  important: true,
+  content: ["./src/**/*.{html,ts}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  darkMode: "class",
+};
